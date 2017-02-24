f18de0e4c058b0ffeb155379fda4e0f046c5f336
Michael Anderson <Micmail566@gmail.com>
2017-02-24 14:07:58 -0500

    Add project files.

---
+++ StarWars.Api.Repository/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace StarWars.Api.Repository
+{
+    [Activity(Label = "StarWars.Api.Repository", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
